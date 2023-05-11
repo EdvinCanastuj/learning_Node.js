c173878d41b4bf3be4c391c2514b0fcc4a140ea9
Edvin Canastuj <ecanastujv@miumg.edu.gt>
2023-05-11 10:27:19 -0600

    primer commit

---
+++ monohilos.js
@@ -0,0 +1,16 @@
+console.log("hola mundo");
+let i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+    /*
+    if (i == 5 ){
+        console.log("forzamos error");
+        var a = 3 + z;
+    }*/
+}, 1000);/*
+console.log("segunda instruccion");
+let nombre = process.env.NOMBRE || "sn nombre";
+let web = process.env.WEB || "sin web ";
+console.log("hola "+ nombre);
+console.log("mi web es  "+ web);*/
---
+++ variables.js
@@ -0,0 +1,5 @@
+console.log("iniciando");
+let nombre= process.env.NOMBRE|| "SIN NOMBRE";
+let web = process.env.WEB||"sin web ";
+console.log("mi nombre es "+nombre);
+console.log("mi web es "+web);
